8d1e142cb2502b7f52b363ebc5c1042183279016
anusha <anusha@LAPTOP-KB5AQG8N>
2022-05-06 09:38:56 +0530

    Organisation creation delete org

---
+++ src/Orgpackage/CreateOrganisation.java
@@ -0,0 +1,8 @@
+package Orgpackage;
+
+public class CreateOrganisation {
+public static void main(String args[])
+{
+	System.out.println("organisation created");
+}
+}
---
+++ src/Orgpackage/DeleteOrganisation.java
@@ -0,0 +1,10 @@
+package Orgpackage;
+
+public class DeleteOrganisation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("delete organisation");
+	}
+
+}
